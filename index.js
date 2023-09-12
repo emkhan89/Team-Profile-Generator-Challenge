fe2bd765352292fde32759b5c35fa691ec2c9c33
unknown <emkhan89>
2023-09-12 22:24:22 +0100

    add inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
+const Engine = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -9,9 +9,31 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+const Employee = require("./lib/Employee");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const staff = [];
 
-
+const starterQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the name of the team manager?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the managers employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the managers email address?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "WHat is the managers office number?",
+  },
+];
